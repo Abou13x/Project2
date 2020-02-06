6a86e443d3bf0043439bf685123f4370cabb8833
Abou Diomande <abodiomande@centares.mvnu.edu>
2020-02-06 16:12:50 -0500

    milestone1

---
+++ Bible.cpp
@@ -0,0 +1,164 @@
+// Bible class function definitions
+// Computer Science, MVNU
+
+#include "Ref.h"
+#include "Verse.h"
+#include "Bible.h" 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+Bible::Bible() { // Default constructor
+	infile = "/home/class/csc3004/Bibles/web-complete";
+}
+
+// Constructor – pass bible filename
+Bible::Bible(const string s) { infile = s; }
+
+// REQUIRED: lookup finds a given verse in this Bible
+const Verse Bible::lookup(Ref ref, LookupResult& status) { 
+    Verse aVerse;
+    // TODO: scan the file to retrieve the line with ref ...
+    // update the status variable,
+	// create and return the verse object
+
+	string buffer, verseText;
+	/* read in every verse */
+
+	if (!isOpen) {
+		instream.open(infile.c_str(), ios::in);
+		if (!instream) {
+			cerr << "Error - can't open input file: " << infile << endl;
+			exit(2);
+		}
+	}
+	else {
+	
+
+			if (currentRef == ref) {
+				aVerse = Verse(currentLine);
+				status = SUCCESS;
+				return(aVerse);
+			}
+
+
+
+
+	}
+
+	
+
+	isOpen = true;
+
+	do {
+		/* get the next verse */
+		getline(instream, buffer); //
+		Ref ref2(buffer);
+		currentLine = buffer;
+		currentRef = ref2;
+		/* See if it is the right book */
+		//ref2.display();
+			if (ref == ref2) {
+				//create a verse
+				aVerse = Verse(buffer);
+				status = SUCCESS;
+				return(aVerse);
+			}
+
+			// if search is not successful
+			if (ref.getBook() > 66 || ref.getBook() < 1) {
+				status = NO_BOOK;
+				return(aVerse);
+			}
+
+			if (ref.getBook() < currentRef.getBook()){ //ref.getBook() < currentRef.getBook()
+				status = NO_CHAPTER;
+				return(aVerse);
+			}
+			if (currentRef.getChap() > ref.getChap() && ref.getBook() == currentRef.getBook()) {
+				status = NO_VERSE;
+				return(aVerse);
+			}
+		
+				
+			
+
+			
+
+			
+	} while (!instream.fail());
+
+	status = OTHER;
+	return(aVerse);
+
+
+   // return(aVerse);
+}
+
+// REQUIRED: Return the reference after the given ref
+const Ref Bible::next(const Ref ref, LookupResult& status) {
+	string buffer, verseText;
+	if (isOpen) {
+		if (currentRef == ref) {
+			getline(instream, buffer);
+			currentLine = buffer;
+			Ref ref3(buffer);
+			status = SUCCESS;
+			currentRef = ref3;
+			return ref3;
+		}
+	}
+	
+	lookup(ref, status);
+
+	if (status == SUCCESS) {
+		if (currentRef == ref) {
+			getline(instream, buffer);
+			currentLine = buffer;
+			Ref ref4(buffer);
+			status = SUCCESS;
+			currentRef = ref4;
+			return ref4;
+		}
+	}
+	 
+
+	Ref ref5;
+	return ref5;
+
+
+	
+};
+
+// OPTIONAL: Return the reference before the given ref
+const Ref prev(const Ref ref, LookupResult& status) {};
+
+// Return an error message string to describe status
+const string Bible::error(LookupResult status) {
+
+	if (status == NO_BOOK) {
+		
+		return "Could not find the book \n";
+	}
+	
+	if (status == NO_CHAPTER) {
+		return "Could not find the chapter \n";
+	}
+	if (status == NO_VERSE) {
+		return "Could not found the verse \n";
+	}
+	if(status == OTHER){
+		return "Could not determine the error \n";
+	}
+	else {
+		cout << "Strange!!!!!!";
+	}
+	
+};
+
+void Bible::display() {
+	cout << "Bible file: " << infile << endl;
+}
---
+++ Ref.h
@@ -0,0 +1,49 @@
+// Class Ref
+// Computer Science, MVNU
+//
+// Ref is a class for representing a Bible reference consisting of
+//    * integer book   - the book from 1 (Genesis) to 66 (Revalation)
+//    * integer chap   - the chapter number >1
+//    * integer verse  - he verse numner >1
+//
+
+#ifndef Ref_H
+#define Ref_H
+#include <string> 
+#include <stdlib.h>
+using namespace std;
+
+// GetNextToken returns a token from str, which contains all characters
+// up to the first character from delimiters
+string GetNextToken(string& str, const string& delimiters);
+
+class Ref {
+private:
+   short book, chap, verse;	// Reference information
+   string bookName[68] = { "Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua", "Judges",
+   "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles", "Ezra", "Nehemiah",
+   "Esther","Job", "Psalms", "Proverbs", "Ecclesiastes", "Song of Solomon", "Isaiah", "Jeremiah", "Lamentations",
+   "Ezekiel", "Daniel", "Hosea","Joel", "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah",
+   "Haggai", "Zechariah", "Malachi", "Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1 Corinthians", "2 Corinthians",
+   "Galatians", "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 thessalonians", "1 Timothy",
+   "2 Timothy", "Titus", "Philemon", "Hebrews", "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude",
+   "Revelation" };
+   string name;
+public:
+   Ref();  	// Default constructor
+   Ref(string s); 	// Parse constructor - example parameter "43:3:16"
+   Ref(const int,const int,const int); 	// Construct from three integers;
+   // Accessors
+   int getBook();	// Access book number
+   int getChap();	// Access chapter number
+   int getVerse();	// Access verse number
+   // REQUIRED Comparison: determine if two references are equal
+   bool operator==(const Ref);
+   // OPTIONAL: < and > comparisons might also be useful
+   
+   void display(); 	// Display the reference on cout, example output: John 3:16
+   // Your version of display must show the book name
+   // corresponding to the stored book number.
+};
+
+#endif //Ref_H
